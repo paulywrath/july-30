a0509022389cd6ecaa47ffb267077c8fe9e930cd
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-30 13:50:30 -0400

    Render list of Daves I know on page

--- server.cjs
+++ server.cjs
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
+const davesIKnow = [`David Hoffner`, `Dave Gord`, `David`, `Dave Jadisky`, `Dave Capisano`]
+
 app.use(express.static('dist'))
 
 app.get('/', (req, res, next) => {
   res.sendFile(__dirname + `/index.html`);
+})
+
+app.get('/api/v1/daves', (req, res, next) => {
+  res.send(davesIKnow);
 })
 
 app.listen(3000, () => {console.log(`server on`)})
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [daves, setDaves] = useState([]);
+
+  useEffect(()=>{
+    const getDaves = async() => {
+      const response = await fetch (`http://localhost:3000/api/v1/daves`);
+      const result = await response.json();
+      setDaves(result);
+    }
+    getDaves();
+  },[])
 
   return (
     <>
       <h1>please work</h1>
+
+      <ul>
+      {
+        daves.map((dave) => {
+          return <li>{dave}</li>
+        }) 
+      }
+      </ul>
     </>
   )
 }
